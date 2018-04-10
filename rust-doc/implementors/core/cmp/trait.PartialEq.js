4d4a2a1adaee65089a26acbf7864ea1975186e6f
Malik Olivier Boussejra <malik@boussejra.com>
2018-04-10 19:09:39 +0900

    rust-doc: Update fitrs to 0.1.1

--- rust-doc/implementors/core/cmp/trait.PartialEq.js
+++ rust-doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["byteorder"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"byteorder/enum.BigEndian.html\" title=\"enum byteorder::BigEndian\">BigEndian</a>",synthetic:false,types:["byteorder::BigEndian"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"byteorder/enum.LittleEndian.html\" title=\"enum byteorder::LittleEndian\">LittleEndian</a>",synthetic:false,types:["byteorder::LittleEndian"]},];
+implementors["fitrs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"fitrs/enum.HeaderValue.html\" title=\"enum fitrs::HeaderValue\">HeaderValue</a>",synthetic:false,types:["fitrs::fits::HeaderValue"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ rust-doc/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fitrs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"fitrs/struct.FitsIntoIter.html\" title=\"struct fitrs::FitsIntoIter\">FitsIntoIter</a>",synthetic:false,types:["fitrs::fits::FitsIntoIter"]},{text:"impl&lt;'f&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"fitrs/struct.FitsIter.html\" title=\"struct fitrs::FitsIter\">FitsIter</a>&lt;'f&gt;",synthetic:false,types:["fitrs::fits::FitsIter"]},{text:"impl&lt;'f&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"fitrs/struct.FitsIterMut.html\" title=\"struct fitrs::FitsIterMut\">FitsIterMut</a>&lt;'f&gt;",synthetic:false,types:["fitrs::fits::FitsIterMut"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
